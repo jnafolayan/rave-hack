20f5e220c0ddfaebd231006c806df3d6823c71ae
John Afolayan <jnafolayan@gmail.com>
2019-10-05 16:22:05 +0100

    fix changes

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,3 @@
-export const API_BASE_URL = 'http://rav-backend.com:8080';
+export const API_BASE_URL = process.env.NODE_ENV == 'production' ? 
+  'http://rav-backend.com:8080' :
+  'http://rav-backend.com';
